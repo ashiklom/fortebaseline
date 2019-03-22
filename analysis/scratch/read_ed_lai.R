616f07196bc9b2e872d01b74eaad0f1d78de61e4
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-03-22 11:25:06 -0400

    Pull `get_monthly_lai` into its own function

--- analysis/scratch/read_ed_lai.R
+++ analysis/scratch/read_ed_lai.R
@@ -6,52 +6,6 @@ library(cowplot)
 
 plan(list(tweak(multiprocess, workers = 8)))
 
-get_monthly_lai <- function(workflow_id, year,
-                            startmonth = NULL) {
-  if (is.null(startmonth)) {
-    startmonth <- 1
-    if (year == 1902) startmonth <- 6
-  }
-
-  nday <- as.numeric(difftime(
-    sprintf("%d-01-01", year + 1),
-    sprintf("%d-%d-01", year, startmonth),
-    tz = "UTC",
-    units = "days"
-  ))
-  nt <- nday * 24 * 2 # Half-hourly output
-
-  filename <- glue::glue("analysis-T-{year}-00-00-000000-g01.h5")
-  filepath <- pecanapi::run_dap(workflow_id, filename)
-
-  nc <- ncdf4::nc_open(filepath)
-  on.exit(ncdf4::nc_close(nc), add = TRUE)
-  lai_raw <- ncdf4::ncvar_get(
-    nc,
-    "LAI_PY",
-    start = c(6, 1, 1, 1),
-    count = c(6, -1, -1, nt) # Only include PFTs 6, (7), 8, 9, 10, 11
-  )[-2,,] # Drop PFT 7
-  pft_levels <- c("Early hardwood", "Mid hardwood", "Late hardwood",
-                  "Pine", "Late conifer")
-  pftnames <- factor(pft_levels[c(4, 5, 1:3)], pft_levels)
-  basedate <- ISOdate(year, startmonth, 1, 0, 0, 0, "UTC")
-
-  data.table::melt(lai_raw) %>%
-    tibble::as_tibble() %>%
-    dplyr::rename(ipft = Var1, cohort = Var2, timestep = Var3) %>%
-    dplyr::mutate(
-      pft = pftnames[ipft],
-      dt = basedate + lubridate::dhours((timestep - 1) / 2),
-      month = lubridate::floor_date(dt, "month")
-    ) %>%
-    dplyr::group_by(month, pft, cohort) %>%
-    dplyr::summarize(lai = mean(value)) %>%
-    dplyr::summarize(lai = sum(lai)) %>%
-    dplyr::ungroup() %>%
-    dplyr::mutate(workflow_id = !!workflow_id)
-}
-
 con <- default_connection()
 
 bety_workflows <- tbl(con, "workflows") %>%
@@ -66,12 +20,14 @@ workflow_df <- bety_workflows %>%
   unnest(configs_df) %>%
   select(-notes)
 
-workflow_ids <- pull(workflow_df, id)
+workflow_ids <- pull(workflow_df, workflow_id)
 
-workflows_years <- as_tibble(expand.grid(
+workflows_years <- expand.grid(
   workflow_id = workflow_ids,
   year = seq(1902, 1912)
-))
+) %>%
+  as_tibble() %>%
+  mutate(startmonth = case_when(year == 1902 ~ 6, TRUE ~ 1))
 
 lai_data <- future_pmap_dfr(
   workflows_years,
