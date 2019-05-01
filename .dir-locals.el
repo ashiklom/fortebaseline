a1f96ad39896909f201a79e739c5395eadcf2946
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-05-01 13:21:48 -0400

    Update Emacs dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,8 @@
 ; Project-specific Emacs configuration
-((ess-r-mode . ((ess-r-package-dirs . (("R" . 1) ("tests" . 1) ("testthat" . 2) ("analysis" . 1) ("scratch" . 2) ("scripts" . 2))))))
+((ess-r-mode . ((ess-r-package-dirs . (("R" . 1) ("tests" . 1) ("testthat" . 2)
+                                       ("analysis" . 1) ("scratch" . 2) ("scripts" . 2)))))
+ (nil . ((projectile-compilation-command . "make"))))
+
+;; Local Variables:
+;; flycheck-disabled-checkers: (emacs-lisp)
+;; End:
