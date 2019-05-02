ac831993a2ac7c367056a77c516a580148868114
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-05-02 15:20:54 -0400

    Add code for adding priors to BETY

--- analysis/scripts/bety_plan.R
+++ analysis/scripts/bety_plan.R
@@ -18,6 +18,7 @@ library(here)
 library(fs)
 library(yaml)
 library(purrr)
+library(readr)
 library(assertthat)
 devtools::load_all(here())
 
@@ -29,9 +30,8 @@ stopifnot(cache$driver$path == cache_path)
 invisible(bety())
 
 # Assign plant functional types in BETY
-define_pfts <- function(pft_names) {
+define_pfts <- function(pft_names, con = bety()) {
   stopifnot(setequal(pft_names, pfts("bety_name")))
-  con <- bety()
   inbety <- tbl(con, "pfts") %>%
     filter(name %in% pft_names) %>%
     collect()
@@ -50,11 +50,10 @@ define_pfts <- function(pft_names) {
 }
 
 # Set the PFT-species relationship in BETY
-set_pft_species <- function(species_df, dryrun = FALSE) {
+set_pft_species <- function(species_df, con = bety(), dryrun = FALSE) {
   stopifnot(setequal(c("species", "pft"), colnames(species_df)))
   input_species <- species_df[["species"]]
   input_pfts <- unique(species_df[["pft"]])
-  con <- bety()
   bety_species <- tbl(con, "species") %>%
     filter(scientificname %in% input_species) %>%
     select(specie_id = id, species = scientificname) %>%
@@ -107,31 +106,69 @@ set_pft_species <- function(species_df, dryrun = FALSE) {
   pfts_species(input_pfts) %>% collect()
 }
 
-set_prior <- function(variable, distn, parama, paramb, pft, overwrite = TRUE) {
-  stop("TODO: Not implemented!")
-  con <- bety()
+set_prior <- function(variable, distn, parama, paramb, pft,
+                      con = bety(), overwrite = TRUE, dryrun = FALSE) {
   # Get variable IDs
-  variable_id <- get_variable_ids(c("nonsense", "leaf_reflect_vis"), con)
+  variable_id <- get_variable_ids(variable, con)
+  pft_id <- get_pft_ids(pft, con)
   # Add any missing variables
-  input <- tidyr::nesting(variable, variable_id, distn, parama, paramb, pft)
-  # Get variable IDs
+  input <- tidyr::nesting(variable, variable_id, distn, parama, paramb, pft, pft_id)
   # Check for existing prior
-  if (overwrite) {
-    # Delete it
+  current_priors <- pfts_priors(pft)
+  existing_priors <- current_priors %>%
+    semi_join(input, by = c("variable", "variable_id", "pft", "pft_id"))
+  if (nrow(existing_priors) > 0) {
+    message("Found ", nrow(existing_priors), " existing priors.")
+    if (overwrite && !dryrun) {
+      # Delete it
+      qry <- DBI::dbSendStatement(con, paste0(
+        "DELETE FROM pfts_priors WHERE ",
+        "pft_id = $1 AND prior_id = $2"
+      ))
+      stmt <- DBI::dbBind(qry, list(
+        existing_priors[["pft_id"]],
+        existing_priors[["prior_id"]]
+      ))
+      DBI::dbClearResult(qry)
+    }
   }
-  # Insert it 
+  new_priors <- input %>%
+    anti_join(current_priors, by = c("variable", "variable_id", "pft", "pft_id"))
+  if (nrow(new_priors) > 0) {
+    message("Inserting ", nrow(new_priors), " new priors.")
+    if (!dryrun) {
+      .success <- DBI::dbWithTransaction(con, {
+        qry <- DBI::dbSendQuery(con, paste0(
+          "INSERT INTO priors (variable_id, distn, parama, paramb, phylogeny) ",
+          "VALUES ($1, $2, $3, $4, 'plants') ",
+          "RETURNING id, variable_id, distn, parama, paramb"
+        ))
+        res <- DBI::dbBind(qry, with(new_priors, list(
+          variable_id, distn, parama, paramb
+        )))
+        prior_insert <- DBI::dbFetch(res)
+        DBI::dbClearResult(qry)
+        qry <- DBI::dbExecute(con, paste0(
+          "INSERT INTO pfts_priors (pft_id, prior_id) ",
+          "VALUES ($1, $2) "
+        ), list(new_priors[["pft_id"]], prior_insert[["id"]]))
+      })
+    }
+  }
+  pfts_priors(pft)
 }
 
 plan <- drake_plan(
   pft_definition = read_yaml(
     file_in(!!here("analysis", "data", "derived-data", "pft_definition.yml"))
   ) %>%
-    purrr::map_dfr(data.frame, stringsAsFactors = FALSE) %>%
+    map_dfr(data.frame, stringsAsFactors = FALSE) %>%
     as_tibble(),
   p_pfts = define_pfts(unique(pft_definition[["pft"]])),
-  p_pfts_species = set_pft_species(pft_definition)
-  # TODO: Add priors
-  # TODO: Add TRY data
+  p_pfts_species = set_pft_species(pft_definition),
+  spec_priors_data = read_csv(file_in(!!here("analysis", "data",
+                                             "derived-data", "priors_spectra.csv"))),
+  set_spec_priors = lift_dl(set_prior)(spec_priors_data)
 )
 
 dconf <- drake_config(
