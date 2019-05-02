5febc29d74fe263e9bc256a3138ba7135d235f13
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-05-02 15:50:41 -0400

    Bugfix in meta analysis script

--- analysis/scripts/run_meta_analysis.R
+++ analysis/scripts/run_meta_analysis.R
@@ -14,5 +14,5 @@ PEcAn.logger::logger.setLevel("WARN")
 outfile <- here::here("analysis", "data", "derived-data", "meta-analysis.rds")
 pfts <- pfts()
 ma_results <- map(pfts[["bety_name"]], pecan_ma_pft, con = con) %>%
-  setNames(ma_results, pfts[["pft"]])
+  setNames(pfts[["pft"]])
 saveRDS(ma_results, outfile)
