3a5268f38ab34fedd6f0c568b65ea74f52ffe175
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-05-07 18:13:01 -0400

    Add notes to meta analysis code

--- analysis/scripts/run_meta_analysis.R
+++ analysis/scripts/run_meta_analysis.R
@@ -1,3 +1,4 @@
+options(conflicts.policy = "depends.ok")
 library(tidyverse)
 library(RPostgres)
 library(fortebaseline)
@@ -10,9 +11,22 @@ con <- dbConnect(
   port = 7990
 )
 
+# NOTE: This will silently drop parameters that don't have a prior (I
+# think?). Need to figure out why that is.
 PEcAn.logger::logger.setLevel("WARN")
 outfile <- here::here("analysis", "data", "derived-data", "meta-analysis.rds")
 pfts <- pfts()
 ma_results <- map(pfts[["bety_name"]], pecan_ma_pft, con = con) %>%
   setNames(pfts[["pft"]])
+
+if (interactive()) {
+  tidy_posterior(ma_results) %>%
+    unnest(draws) %>%
+    mutate(pft = factor(pft, pfts[["pft"]])) %>%
+    ggplot() +
+    aes(x = pft, y = draws, fill = pft) +
+    geom_violin() +
+    facet_wrap(vars(trait), scales = "free")
+}
+
 saveRDS(ma_results, outfile)
