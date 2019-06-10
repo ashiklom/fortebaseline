4b680ac47e38bfc5b15573090c8a8fb9cdda4e9b
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-06-10 08:06:26 -0400

    Update run-status script

--- analysis/scripts/run-status.R
+++ analysis/scripts/run-status.R
@@ -1,8 +1,19 @@
 #!/usr/bin/env Rscript
 library(tidyverse)
+library(lubridate, exclude = "here")
+library(here)
+
+current_wf <- here("analysis", "data",
+                   "derived-data", "current-workflows.csv") %>%
+  read_csv(col_types = c("ddlll"))
+
+
+wf_string <- sprintf("{%s}", paste(current_wf$workflow_id, collapse = ","))
+
 wf <- system2("ssh", c("pecan", "find",
-                        "/public/shared-docker-volumes/pecan_data/workflows/PEcAn_99000000{144..151}",
-                        "-name", "analysis-I-*"),
+                       paste0("/public/shared-docker-volumes/pecan_data/workflows/PEcAn_",
+                              wf_string),
+                       "-name", "analysis-I-*"),
                stdout = TRUE)
 
 wf_data <- wf %>%
@@ -18,5 +29,6 @@ wf_data <- wf %>%
 wf_data %>%
   group_by(workflow, run) %>%
   filter(date == max(date)) %>%
-  select(-raw) %>%
-  print(n = Inf)
+  ungroup() %>%
+  group_by(workflow, date_lt = ceiling_date(date, "25 years")) %>%
+  count()
