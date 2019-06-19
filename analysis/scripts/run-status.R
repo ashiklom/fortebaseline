f67de675aace31f28229250089aedf2ce02e090f
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-06-19 12:21:43 -0400

    Use `current_workflows` object in run-status

--- analysis/scripts/run-status.R
+++ analysis/scripts/run-status.R
@@ -1,14 +1,10 @@
 #!/usr/bin/env Rscript
+library(fortebaseline)
 library(tidyverse)
 library(lubridate, exclude = "here")
 library(here)
 
-current_wf <- here("analysis", "data",
-                   "derived-data", "current-workflows.csv") %>%
-  read_csv(col_types = c("ddlll"))
-
-
-wf_string <- sprintf("{%s}", paste(current_wf$workflow_id, collapse = ","))
+wf_string <- sprintf("{%s}", paste(current_workflows$workflow_id, collapse = ","))
 
 wf <- system2("ssh", c("pecan", "find",
                        paste0("/public/shared-docker-volumes/pecan_data/workflows/PEcAn_",
