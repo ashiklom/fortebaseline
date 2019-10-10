cc301d8606ad29caef6335857ab7dd879b42c675
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-10-10 16:06:29 -0400

    Add PFT ternary plot, and other scratch code

--- analysis/drake/overall-uncertainty.R
+++ analysis/drake/overall-uncertainty.R
@@ -27,24 +27,43 @@ plan <- plan <- bind_plans(plan, drake_plan(
   pft_totals = pft_data %>%
     group_by(case, year) %>%
     summarize_at(vars(-pft, -agb_frac), sum) %>%
-    pivot_longer(-c(case, year), names_to = "variable", values_to = "value"),
+    pivot_longer(-c(case, year), names_to = "variable", values_to = "value")
+))
+
+### Summary plot
+plan <- bind_plans(plan, drake_plan(
   observations = tribble(
     ~variable, ~low, ~mean, ~hi,
     "npp", 6, 6.5, 7,
     "lai", 3.97 - 1.96 * 0.423, 3.97, 3.97 + 1.96 * 0.423
   ) %>%
     expand_grid(model = models$model),
-  summary_ts_plot_gg = bind_rows(scalar_data, pft_totals) %>%
+  ts_both = bind_rows(scalar_data, pft_totals) %>%
     mutate(
       model_id = substr(case, 4, 6),
       variable = stringr::str_remove(variable, "^mmean_") %>%
         stringr::str_remove("_py$")
     ) %>%
-    filter(variable %in% c("npp", "lai")) %>%
+    filter(variable %in% c("npp", "lai")),
+  ts_summary = ts_both %>%
+    group_by(model_id, year, variable) %>%
+    summarize(
+      hi = quantile(value, 0.9),
+      lo = quantile(value, 0.1)
+    ) %>%
+    left_join(models, "model_id"),
+  summary_ts_plot_gg = ts_both %>%
     left_join(models, "model_id") %>%
     ggplot() +
-    aes(x = year, y = value, color = color, group = case) +
-    geom_line(alpha = 0.25) +
+    aes(x = year, color = color) +
+    geom_line(aes(y = value, group = case), alpha = 0.1, size = 0.3) +
+    geom_ribbon(
+      aes(ymin = lo, ymax = hi),
+      data = ts_summary,
+      color = "black",
+      fill = NA,
+      linetype = "dashed"
+    ) +
     geom_pointrange(
       aes(x = 2000, y = mean, ymin = low, ymax = hi),
       data = observations,
@@ -69,8 +88,7 @@ plan <- plan <- bind_plans(plan, drake_plan(
       axis.title = element_blank(),
       axis.text.x = element_text(angle = 90),
       strip.background = element_blank(),
-      strip.placement = "outside",
-
+      strip.placement = "outside"
     ),
   summary_ts_plot_png = ggsave(
     file_out("analysis/figures/summary-ts-plot.png"),
@@ -82,34 +100,144 @@ plan <- plan <- bind_plans(plan, drake_plan(
   ))
 ))
 
+### PFT plot
+plan <- bind_plans(plan, drake_plan(
+  pft_jitterplot_gg = pft_data %>%
+    filter(year %in% c(1910, 1920, 1950, 1980)) %>%
+    mutate(year = factor(year),
+           model_id = substr(case, 4, 6)) %>%
+    left_join(models, "model_id") %>%
+    ggplot() +
+    aes(x = year, y = agb_frac, color = pft) +
+    geom_jitter(size = 0.5, alpha = 0.2, position = position_jitterdodge()) +
+    facet_wrap(vars(model), ncol = 2) +
+    scale_color_manual(values = pfts("color")) +
+    labs(y = "AGB fraction", color = "PFT") +
+    guides(color = guide_legend(override.aes = list(alpha = 1, size = 2))) +
+    theme_cowplot() +
+    theme(
+      axis.title.x = element_blank(),
+      axis.text.x = element_text(angle = 90),
+      strip.background = element_blank(),
+      legend.position = "bottom"
+    ),
+  pft_jitterplot_png = ggsave(
+    file_out("analysis/figures/pft-jitterplot.png"),
+    pft_jitterplot_gg,
+    width = 6.6, height = 6.4
+  ),
+  pft_jitterplot_knit = knitr::include_graphics(file_in(
+    "analysis/figures/pft-jitterplot.png"
+  ))
+))
+
+### PFT ternary plot
+plan <- bind_plans(plan, drake_plan(
+  tern_data = pft_data %>%
+    mutate(model_id = substr(case, 4, 6)) %>%
+    filter(year %in% c(1920, 1950, 1980)) %>%
+    select(case, model_id, year, pft, agb_frac) %>%
+    pivot_wider(names_from = "pft", values_from = "agb_frac") %>%
+    mutate(`Mid/Late` = `Mid hardwood` + `Late hardwood`) %>%
+    select(-`Mid hardwood`, -`Late hardwood`) %>%
+    rename("Early" = "Early hardwood") %>%
+    left_join(models, "model_id"),
+  pft_tern_gg = ggplot(tern_data) +
+    aes(x = Early, y = `Mid/Late`, z = Pine) +
+    ggtern::geom_tri_tern(bins = 10) +
+    facet_grid(
+      vars(year),
+      vars(fct_relabel(model, ~gsub(" ", "\n", .)))
+    ) +
+    scale_fill_viridis_c(na.value = "white") +
+    guides(fill = guide_colorbar(title = "Count")) +
+    ggtern::coord_tern() +
+    theme(
+      axis.title = element_blank()
+    ),
+  pft_tern_png = ggsave(
+    file_out("analysis/figures/pft-tern.png"),
+    pft_tern_gg,
+    width = 10, height = 4
+  ),
+  pft_tern_knit = knitr::include_graphics(file_in(
+    "analysis/figures/pft-tern.png"
+  ))
+))
+
 stop()
+
+### Pairs plots
+pft_wide = pft_data %>%
+  mutate(model_id = substr(case, 4, 6)) %>%
+  select(case, pft, year, model_id, agb_frac) %>%
+  setDT() %>%
+  dcast(case + year + model_id ~ pft, value.var = "agb_frac") %>%
+  as_tibble()
+
+both_wide = scalar_data %>%
+  setDT() %>%
+  dcast(case + year ~ variable, value.var = "value") %>%
+  as_tibble() %>%
+  left_join(pft_wide, c("case", "year"))
+
+both_wide %>%
+  filter(year == 1920) %>%
+  select(model_id, starts_with("mmean"), `Early hardwood`:`Pine`) %>%
+  GGally::ggpairs(
+    .,
+    aes(color = model_id),
+    size = 0.5,
+    alpha = 0.2,
+    columns = names(.)[-1]
+  )
+
+tern_data %>%
+  filter(year == 1920, model_id == "FMS") %>%
+  ggplot() +
+  aes(x = Early, y = `Mid/Late`, z = Pine) +
+  ggtern::geom_tri_tern(bins = 10) +
+  scale_fill_viridis_c(na.value = "white") +
+  ggtern::coord_tern()
+
+get_density <- function(x, y, z, ...) {
+  dens <- misc3d::kde3d(x, y, z, ...)
+  ix <- findInterval(x, dens$x)
+  iy <- findInterval(y, dens$y)
+  iz <- findInterval(z, dens$z)
+  ii <- cbind(ix, iy, iz)
+  dens$d[ii]
+}
+
+pft_tern_pl
+
+tern_data2 <- tern_data %>%
+  group_by(model_id) %>%
+  mutate(dens = get_density(`Early hardwood`, `ML`, `Pine`)) %>%
+  ungroup()
+ggplot(tern_data2) +
+  aes(x = `Early hardwood`, y = `ML`, z = `Pine`, color = dens) +
+  geom_point() +
+  facet_wrap(vars(model_id), ncol = 2) +
+  scale_color_viridis_c() +
+  ggtern::coord_tern()
 
 
 pft_data %>%
-  filter(year %in% c(1910, 1920, 1950, 1980)) %>%
-  mutate(year = factor(year)) %>%
+  mutate(model_id = substr(case, 4, 6)) %>%
+  left_join(models, "model_id") %>%
   ggplot() +
-  aes(x = year, y = agb_frac, fill = pft, color = pft) +
-  geom_jitter(size = 0.5, alpha = 0.2, position = position_jitterdodge()) +
-  facet_wrap(vars(model_id), ncol = 2)
-
-ggplot(pft_data) +
-  aes(x = year, y = agb_frac, group = case) +
-  geom_line(alpha = 0.1, size = 0.2) +
-  facet_grid(vars(pft), vars(model_id))
-
-scalar_data %>%
-  left_join(cases, "case") %>%
-  ggplot() +
-  aes(x = year, y = value, group = case) +
-  geom_line(alpha = 0.2, size = 0.3) +
-  facet_grid(vars(variable), vars(model_id), scales = "free_y")
-
-ggplot(scalar_data) +
-  aes(x = year, y = value, group = case) +
-  geom_line() +
-  facet_grid(vars(variable), vars(case))
-scalar_data
+  aes(x = year, y = agb_frac, group = case, color = color) +
+  geom_line(alpha = 0.2, size = 0.2) +
+  facet_grid(vars(pft), vars(fct_relabel(model, ~gsub(" ", "\n", .)))) +
+  labs(y = "AGB fraction") +
+  scale_color_identity() +
+  theme_cowplot() +
+  theme(
+    axis.title.x = element_blank(),
+    axis.text.x = element_text(angle = 90),
+    strip.background = element_blank()
+  )
 
 ### Download files
 cohort_osf <- "..."
