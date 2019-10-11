11e56efc94459eba193bd53c7c3113dfef95f06a
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-10-11 17:14:19 -0400

    Code for sensitivity analysis

--- R/sensitivity_analysis.R
+++ R/sensitivity_analysis.R
@@ -4,18 +4,23 @@
 #' Need to fit one at a time.
 #' @param y Response variable
 #' @param x Dependent variable
+#' @param ymedian Median of response variable (default is calculated as
+#'   `median(y)`)
+#' @param xmedian Median of dependent variable (default is calculated as
+#'   `median(x)`)
 #' @param alpha Fraction of median around which to calculate
-#' derivative for sensitivity. Default = 0.01
+#' derivative for sensitivity. Default = 0.05
 #' @return `tibble` of sensitivity analysis results, with columns
 #' `cv` (Coefficient of variation for `x`), `sensitivity`
 #' (unnormalized sensitivity of `y` to `x` at median), `elasticity`
 #' (normalized `sensitivity`), and `pvar` (prediction variance).
 #' @export
-sensitivity_analysis <- function(y, x, alpha = 0.01) {
+sensitivity_analysis <- function(y, x,
+                                 ymedian = median(y),
+                                 xmedian = median(x),
+                                 alpha = 0.05) {
   stopifnot(is.numeric(y), is.numeric(x))
   fit <- mgcv::gam(y ~ s(x))
-  xmedian <- median(x)
-  ymedian <- median(y)
   # CV -- Normalized parameter variance
   cv <- var(x) / xmedian
   # Sensitivity -- derivative at the median
---
+++ analysis/drake/sensitivity-analysis.R
@@ -0,0 +1,173 @@
+### Median results for sensitivity analysis
+plan <- bind_plans(plan, drake_plan(
+  median_fluxes = median_results %>%
+    select(casename, scalar) %>%
+    unnest(scalar) %>%
+    select(case, param_id, datetime, mmean_npp_py) %>%
+    group_by(case, param_id, year = year(datetime)) %>%
+    summarize(mmean_npp_py = mean(mmean_npp_py)) %>%
+    ungroup() %>%
+    mutate(model_id = stringr::str_sub(case, -3, -1)),
+  median_pft = median_results %>%
+    select(casename, pft_py) %>%
+    unnest(pft_py) %>%
+    select(case, param_id, datetime, pft, agb_py, mmean_lai_py) %>%
+    mutate(
+      pft = set_pft(pft),
+      model_id = stringr::str_sub(case, -3, -1)
+    ) %>%
+    filter(month(datetime) >= 6, month(datetime) <= 8) %>%
+    group_by(case, param_id, year = year(datetime), pft) %>%
+    select(-datetime) %>%
+    summarize_all(mean) %>%
+    mutate(frac_agb = agb_py / sum(agb_py)) %>%
+    ungroup(),
+  median_lai = median_pft %>%
+    group_by(case, param_id, year) %>%
+    summarize(mmean_lai_py = sum(mmean_lai_py)) %>%
+    ungroup(),
+  median_pft_frac = median_pft %>%
+    select(case, param_id, year, pft, frac_agb) %>%
+    mutate(pft = factor(pft, levels(pft), c("EH", "MH", "LH", "Pine"))) %>%
+    setDT() %>%
+    dcast(case + param_id + year ~ pft, value.var = "frac_agb") %>%
+    as_tibble(),
+  median_params = median_results[["trait_values"]][[1]] %>%
+    bind_rows(.id = "pft") %>%
+    mutate(pft = factor(pft, pfts("bety_name"), pfts("shortname"))) %>%
+    pivot_longer(-pft) %>%
+    unite(variable, c(pft, name), sep = ".") %>%
+    pivot_wider(names_from = "variable", values_from = "value") %>%
+    mutate(param_id = -1),
+  median_wide = median_fluxes %>%
+    left_join(median_lai, c("case", "param_id", "year")) %>%
+    left_join(median_pft_frac, c("case", "param_id", "year")) %>%
+    left_join(models, "model_id") %>%
+    mutate(param_id = -1) %>%
+    left_join(median_params, "param_id")
+))
+
+### Inputs for sensitivity analysis
+do_sensitivity <- function(y, x, ymedian, xmedian) {
+  tryCatch(
+    list(sensitivity_analysis(y, x, ymedian[1], xmedian[1])),
+    error = function(e) NULL
+  )
+}
+
+plan <- bind_plans(plan, drake_plan(
+  sensitivity_vars = c("mmean_npp_py", "mmean_lai_py", "EH", "MH", "LH", "Pine"),
+  sensitivity_groups = bind_rows(
+    tibble(year = 1920:1950, sgroup = "1920-1950"),
+    tibble(year = 1975:1999, sgroup = "1975-1999")
+  ),
+  params_wide = params %>%
+    rename(pft = name) %>%
+    mutate(pft = factor(pft, pfts("bety_name"), pfts("shortname"))) %>%
+    pivot_longer(-c(param_id, pft)) %>%
+    unite(variable, c(pft, name), sep = ".") %>%
+    pivot_wider(names_from = "variable", values_from = "value"),
+  sensitivity_inputs = both_wide %>%
+    inner_join(sensitivity_groups, "year") %>%
+    rename(
+      EH = `Early hardwood`,
+      MH = `Mid hardwood`,
+      LH = `Late hardwood`
+    ) %>%
+    select(case, model_id, param_id, sgroup, sensitivity_vars) %>%
+    group_by(case, model_id, param_id, sgroup) %>%
+    summarize_all(mean, na.rm = TRUE) %>%
+    ungroup() %>%
+    pivot_longer(sensitivity_vars, names_to = "yvar", values_to = "y") %>%
+    left_join(params_wide, "param_id") %>%
+    pivot_longer(-c(case, model_id, param_id, sgroup, yvar, y),
+                 names_to = "xvar", values_to = "x"),
+  median_sens_inputs = median_wide %>%
+    inner_join(sensitivity_groups, "year") %>%
+    select(model_id, sgroup, sensitivity_vars,
+           matches(paste(pfts("shortname"), collapse = "|"))) %>%
+    group_by(model_id, sgroup) %>%
+    summarize_all(mean, na.rm = TRUE) %>%
+    ungroup() %>%
+    pivot_longer(
+      sensitivity_vars,
+      names_to = "yvar",
+      values_to = "ymedian"
+    ) %>%
+    pivot_longer(
+      -c(model_id, sgroup, yvar, ymedian),
+      names_to = "xvar",
+      values_to = "xmedian"
+    ),
+  combined_sens_inputs = sensitivity_inputs %>%
+    left_join(median_sens_inputs, c("model_id", "sgroup", "yvar", "xvar")),
+  sensitivity_raw_results = combined_sens_inputs %>%
+    group_by(model_id, sgroup, yvar, xvar) %>%
+    summarize(result = list(do_sensitivity(y, x, ymedian, xmedian)))
+))
+
+### STOP HERE
+stop()
+
+saveRDS(sensitivity_raw_results, "analysis/data/retrieved/sensitivity-raw-results.rds")
+
+sensitivity_proc = sensitivity_raw_results %>%
+  unnest(result) %>%
+  unnest(result)
+
+sensitivity_proc %>%
+  ungroup() %>%
+  filter(sgroup == sgroup[1]) %>%
+  group_by(model_id, yvar) %>%
+  arrange(desc(pvar), .by_group = TRUE) %>%
+  slice(1:5) %>%
+  separate(xvar, c("pft", "param"), extra = "merge")
+
+
+sensitivity_proc %>%
+  ungroup() %>%
+  filter(is.finite(elasticity)) %>%
+  arrange(desc(pvar))
+
+summary(sensitivity_proc)
+
+s <- combined_sens_inputs %>%
+  group_by(model_id, sgroup, yvar, xvar) %>%
+  group_split()
+
+s1 <- s[[1]]
+s1 %>%
+  select(y, x, ymedian, xmedian) %>%
+  pmap()
+sensitivity_analysis(s1$y, s1$x, s1$ymedian[1], s1$xmedian[1])
+
+
+sensitivity_results = sensitivity_inputs %>%
+  group_by(model_id, sgroup, yvar, xvar) %>%
+  summarize(sens_out = sensitivity_analysis(y, x, ))
+
+both_wide
+
+model <- both_wide$model[1]
+year <- 1920
+
+median_yr = median_wide %>%
+  filter(year == !!year, model == !!model) %>%
+  select(param_id, year, !!sensitivity_vars)
+
+
+s <- sensitivity_analysis()
+
+both_wide
+
+dsub <- both_wide %>%
+  filter(model == !!model, year == !!year) %>%
+  select(
+    param_id,
+    mmean_npp_py, mmean_lai_py,
+    EH = `Early hardwood`,
+    MH = `Mid hardwood`,
+    LH = `Late hardwood`,
+    Pine
+  ) %>%
+  left_join()
