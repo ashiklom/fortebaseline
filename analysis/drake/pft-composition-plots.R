3600b2c80513f6bb57d3b109e5e7a1b7add7d92f
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2020-03-13 17:37:05 -0400

    Replace PFT plot with histogram

--- analysis/drake/pft-composition-plots.R
+++ analysis/drake/pft-composition-plots.R
@@ -1,59 +1,36 @@
 ### PFT plot
 plan <- bind_plans(plan, drake_plan(
-  pft_jitterplot_gg = pft_data %>%
+  pft_histogram = pft_data %>%
     filter(year %in% c(1910, 1920, 1950, 1980)) %>%
-    mutate(year = factor(year),
-           model_id = substr(case, 4, 6)) %>%
+    mutate(
+      year = factor(year),
+      model_id = substr(case, 4, 6),
+      agb_frac_bin = cut(agb_frac, c(-Inf, 0, 0.25, 0.5, 0.75, 0.999, 1),
+                         labels = c("0", "(0,0.25]", "(0.25, 0.5]", "(0.5, 0.75]",
+                                    "(0.75, 1)", "1"))
+    ) %>%
+    group_by(pft, year, model_id, agb_frac_bin) %>%
+    summarize(nagb_frac = n()) %>%
+    ungroup() %>%
     left_join(models, "model_id") %>%
     ggplot() +
-    aes(x = year, y = agb_frac, color = pft) +
-    geom_jitter(size = 0.5, alpha = 0.2, position = position_jitterdodge()) +
-    facet_wrap(vars(model), ncol = 2) +
-    scale_color_manual(values = pfts("color")) +
-    labs(y = "AGB fraction", color = "PFT") +
-    guides(color = guide_legend(override.aes = list(alpha = 1, size = 2))) +
-    theme_cowplot() +
+    aes(x = agb_frac_bin, y = nagb_frac, fill = pft) +
+    geom_col(position = "dodge") +
+    facet_grid(vars(year), vars(model),
+               labeller = labeller(model = label_wrap_gen(10))) +
+    labs(x = "AGB Fraction", y = "Count", fill = "PFT") +
+    scale_fill_manual(values = pfts("color")) +
+    theme_bw() +
     theme(
-      axis.title.x = element_blank(),
-      axis.text.x = element_text(angle = 90),
+      text = element_text(size = 14),
+      axis.text.x = element_text(angle = 90, vjust = 0.5),
       strip.background = element_blank(),
       legend.position = "bottom"
     ),
-  pft_jitterplot_png = ggsave(
-    file_out("analysis/figures/pft-jitterplot.png"),
-    pft_jitterplot_gg,
-    width = 6.6, height = 6.4
-  )
-))
-
-### PFT ternary plot
-plan <- bind_plans(plan, drake_plan(
-  tern_data = pft_data %>%
-    mutate(model_id = substr(case, 4, 6)) %>%
-    filter(year %in% c(1920, 1950, 1980)) %>%
-    select(case, model_id, year, pft, agb_frac) %>%
-    pivot_wider(names_from = "pft", values_from = "agb_frac") %>%
-    mutate(`Mid/Late` = `Mid hardwood` + `Late hardwood`) %>%
-    select(-`Mid hardwood`, -`Late hardwood`) %>%
-    rename("Early" = "Early hardwood") %>%
-    left_join(models, "model_id"),
-  pft_tern_gg = ggplot(tern_data) +
-    aes(x = Early, y = `Mid/Late`, z = Pine) +
-    ggtern::geom_tri_tern(bins = 10) +
-    facet_grid(
-      vars(year),
-      vars(fct_relabel(model, ~gsub(" ", "\n", .)))
-    ) +
-    scale_fill_viridis_c(na.value = "white") +
-    guides(fill = guide_colorbar(title = "Count")) +
-    ggtern::coord_tern() +
-    theme(
-      axis.title = element_blank()
-    ),
-  pft_tern_png = ggsave(
-    file_out("analysis/figures/pft-tern.png"),
-    pft_tern_gg,
-    width = 10, height = 4
+  pft_histogram_png = ggsave(
+    file_out("analysis/figures/pft-histogram.png"),
+    pft_histogram,
+    width = 10.8, height = 7.5
   )
 ))
 
